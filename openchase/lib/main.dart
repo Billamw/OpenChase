0ca1a2af9d7fb3bcb3da02895452efb3e7178f34
Johan Dasbach <johan.dasbach@gmx.de>
2025-03-22 00:56:25 +0100

    added main screen and create room screen

--- openchase/lib/main.dart
+++ openchase/lib/main.dart
@@ -1,89 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:openchase/main_screen.dart';
 
 void main() {
   runApp(const OpenChaseApp());
 }
 
 class OpenChaseApp extends StatelessWidget {
-  const OpenChaseApp({super.key});
+  const OpenChaseApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'OpenChase',
+      title: 'Open Chase',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
+        // Your theme configuration
       ),
-      home: const OpenChaseMainScreen(),
-    );
-  }
-}
-
-class OpenChaseMainScreen extends StatelessWidget {
-  const OpenChaseMainScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final brightness = Theme.of(context).brightness;
-    final String logoPath =
-        brightness == Brightness.light
-            ? 'images/logo_light_1024.png'
-            : 'images/logo_dark_1024.png';
-
-    return Scaffold(
-      body: Column(
-        children: [
-          // Logo at the top
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Image.asset(logoPath, width: 200),
-          ),
-          // Create Room and Join Room buttons
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Handle create room button press
-                    },
-                    child: const Text('Create Room'),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Handle join room button press
-                    },
-                    child: const Text('Join Room'),
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-          // Settings icon at the bottom
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: IconButton(
-                iconSize: 32,
-                onPressed: () {
-                  // Handle settings button press
-                },
-                icon: const Icon(Icons.settings),
-              ),
-            ),
-          ),
-        ],
-      ),
+      home: const MainScreen(),
     );
   }
 }
