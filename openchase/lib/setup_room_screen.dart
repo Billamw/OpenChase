f2b11411a715068fca5088728af29a4cafd6c858
Johan Dasbach <johan.dasbach@gmx.de>
2025-03-26 09:15:11 +0100

    ignored another problem

--- openchase/lib/setup_room_screen.dart
+++ openchase/lib/setup_room_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:openchase/invite_room_screen.dart';
 import 'package:openchase/utils/ui_helper.dart';
